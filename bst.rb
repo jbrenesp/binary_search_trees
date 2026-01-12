c680951c78427a81401dccd4015d6a1a36080f54
jbrenesp <josembrenes@gmail.com>
2026-01-12 11:35:04 +0200

    Add Tree class with initialize and build_tree placeholder

--- bst.rb
+++ bst.rb
@@ -13,3 +13,18 @@ class Node
     data <=> other.data
   end
 end
+
+class Tree
+  
+  def initialize(array)
+    #accepts an array
+   @array = array
+   #it should have a root attribute
+   @root = build_tree(array)
+   #root uses the return value of tree
+  end
+
+  def build_tree(array)
+  end
+
+end
